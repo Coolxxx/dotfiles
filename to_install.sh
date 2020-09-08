a069dcb2e0df97585a7af23249941b3ab084524b
Coolxxx <26342091+Coolxxx@users.noreply.github.com>
2020-09-07 21:40:34 -0700

    add some install

--- to_install.sh
+++ to_install.sh
@@ -1,4 +1,6 @@
+#!/bin/bash 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install wget
 brew install zsh
 brew install zsh-syntax-highlighting
 brew install zsh-autosuggestions
@@ -12,5 +14,15 @@ brew install bash-completion
 brew install python@3.8
 brew install pyenv
 
-
+# optional: 
 # brew install ffmpeg
+# brew install opencv
+# vim rainbow Parentheses: https://github.com/luochen1990/rainbow
+
+# Applications:
+# chrome
+# Sublime Text 3
+# Notion
+
+# Other:
+# vimrc plugin: https://github.com/amix/vimrc
